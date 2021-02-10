efc4a3e06d22753dd34f4c8d14883a2f18091d56
Hannah <40068760+hanadrizz@users.noreply.github.com>
2021-02-10 19:28:39 +0100

    saves pastes to files now

--- trashpanda.py
+++ trashpanda.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 from html.parser import HTMLParser
+import time
+import os
 
+path = "pastes\\"
 # just so bot doesnt get locked out
 headers = {
     'Access-Control-Allow-Origin': '*',
@@ -11,35 +14,61 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
-#check the archived posts
-pastebin = requests.get("https://pastebin.com/archive", headers)
+while True:
+    alarchives = []
 
-#find all links to pastebin.com's pastes
-archive = BeautifulSoup(pastebin.content, 'html.parser')
-findarchive = archive.find_all("a", href=True)
-pastecount = 0
-archives = []
-# take the ones that dont start with /about/ or are for other matters
-for x in findarchive:
-    pastecount += 1
-    print(f"Paste #{pastecount}: {x['href']}")
+    with open("alarchives.txt", "a+") as falarchives:
+        text = falarchives.read()
+    alarchives = text.split("\n")
+    falarchives.close()
+
+
+    #check the archived posts
+    pastebin = requests.get("https://pastebin.com/archive", headers)
+
+    #find all links to pastebin.com's pastes
+    archive = BeautifulSoup(pastebin.content, 'html.parser')
+    findarchive = archive.find_all("a", href=True)
+    pastecount = 0
+    archives = []
+    # take the ones that dont start with /about/ or are for other matters
+    for x in findarchive:
+        pastecount += 1
+        print(f"Paste #{pastecount}: {x['href']}")
+        print("#"*25)
+        a = x['href']
+        if len(a) == 9:
+            archives.append(a)
+
+    #Just to print out the paste urls
+    print("\n\n\n")
+    for y in archives:
+        print("#"*25)
+        print(f"Paste link: {y}")
     print("#"*25)
-    a = x['href']
-    if len(a) == 9:
-        archives.append(a)
-
-print("\n\n\n")
-for y in archives:
+    print("\n\n\n")
     print("#"*25)
-    print(f"Paste link: {y}")
-print("#"*25)
-print("\n\n\n")
-print("#"*25)
 
-# for j in archives:
-#     url = "https://pastebin.com" + str(j)
-#     paste = requests.get(url)
-#     getpaste = BeautifulSoup(paste.content, "html.parser")
-#     print("#"*25)
 
-print(len(archives))
+    # Get the raw data for each entry in the archives
+    for j in archives:
+        if j not in alarchives:
+            alarchives.append(j)
+        elif j in alarchives:
+            continue
+        url = "https://pastebin.com" + str(j)
+        paste = requests.get(url)
+        getpaste = BeautifulSoup(paste.content, "html.parser")
+        rawpaste = getpaste.find(class_="textarea", text=True)
+        pastefile = j[1:]
+        
+        with open(path+pastefile+".txt", "wb") as pfile:
+            pfile.write(rawpaste.encode("utf-8"))
+        pfile.close()
+
+
+    with open('alarchives.txt', 'w') as falarchives:
+        for item in alarchives:
+            falarchives.write("%s\n" % item)
+    falarchives.close()
+    time.sleep(60)
