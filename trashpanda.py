32817abe5f6282d9278b0e5896070766822c5127
Hannah <40068760+hanadrizz@users.noreply.github.com>
2021-02-10 20:43:07 +0100

    wasnt storing already saved pastes to array correctly

--- trashpanda.py
+++ trashpanda.py
@@ -14,14 +14,16 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
     }
 
+def readFile(fileName):
+        fileObj = open(fileName, "r") #opens the file in read mode
+        words = fileObj.read().splitlines() #puts the file into an array
+        fileObj.close()
+        return words
+
 while True:
     alarchives = []
 
-    with open("alarchives.txt", "a+") as falarchives:
-        text = falarchives.read()
-    alarchives = text.split("\n")
-    falarchives.close()
-
+    alarchives = readFile("alarchives.txt")
 
     #check the archived posts
     pastebin = requests.get("https://pastebin.com/archive", headers)
@@ -34,41 +36,31 @@ while True:
     # take the ones that dont start with /about/ or are for other matters
     for x in findarchive:
         pastecount += 1
-        print(f"Paste #{pastecount}: {x['href']}")
-        print("#"*25)
         a = x['href']
         if len(a) == 9:
             archives.append(a)
-
-    #Just to print out the paste urls
-    print("\n\n\n")
-    for y in archives:
-        print("#"*25)
-        print(f"Paste link: {y}")
-    print("#"*25)
-    print("\n\n\n")
-    print("#"*25)
-
 
     # Get the raw data for each entry in the archives
     for j in archives:
         if j not in alarchives:
             alarchives.append(j)
+
+            url = "https://pastebin.com" + str(j)
+            paste = requests.get(url)
+            getpaste = BeautifulSoup(paste.content, "html.parser")
+            rawpaste = getpaste.find(class_="textarea", text=True)
+            pastefile = j[1:]
+            
+            with open(path+pastefile+".txt", "wb") as pfile:
+                pfile.write(rawpaste.encode("utf-8"))
+            pfile.close()
+            print(f"{pfile} written to disk.")
+
         elif j in alarchives:
             continue
-        url = "https://pastebin.com" + str(j)
-        paste = requests.get(url)
-        getpaste = BeautifulSoup(paste.content, "html.parser")
-        rawpaste = getpaste.find(class_="textarea", text=True)
-        pastefile = j[1:]
-        
-        with open(path+pastefile+".txt", "wb") as pfile:
-            pfile.write(rawpaste.encode("utf-8"))
-        pfile.close()
-
 
     with open('alarchives.txt', 'w') as falarchives:
         for item in alarchives:
             falarchives.write("%s\n" % item)
     falarchives.close()
-    time.sleep(60)
+    time.sleep(10)
